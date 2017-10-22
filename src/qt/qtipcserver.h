f1429dde2e33f005f596da24fdffb2eea5454c72
CryptotradeGMO <32884932+CryptotradeGMO@users.noreply.github.com>
2017-10-23 01:38:01 +0200

    Add files via upload

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define GEMCO-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "GEMCOURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
